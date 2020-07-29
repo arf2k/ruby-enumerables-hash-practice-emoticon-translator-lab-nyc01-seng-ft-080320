a08af29de98458d0358583a3aec44e4e6edc883c
Alex Foreman <foreman.alexander@gmail.com>
2020-07-29 19:23:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,6 +15,7 @@ end
 def get_english_meaning(emoticon)
   library = load_library(file)
     word = library.keys.find do |key|
+      library[key][:english] == emoticon 
       binding.pry 
     end
   end 
